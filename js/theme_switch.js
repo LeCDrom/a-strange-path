e0221bcb22627225476ce920d8b295d0488d89d1
Côme Dairin <dairincome@gmail.com>
2024-12-24 15:05:41 +0100

    Fixed conflict between the two toggle-theme buttons

--- js/theme_switch.js
+++ js/theme_switch.js
@@ -1,6 +1,8 @@
 const themeToggleButton = document.getElementById('theme-toggle');
+const simple_themeToggleButton = document.getElementById('simple-theme-toggle');
 const bodyElement = document.body;
 const themeIcon = themeToggleButton.querySelector('i'); // Select the icon inside the button
+const simple_themeIcon = simple_themeToggleButton.querySelector('i'); // Select the icon inside the button
 
 // Check for saved theme preference in localStorage and apply it
 let currentTheme = localStorage.getItem('theme') || 'dark-theme';
@@ -21,11 +23,28 @@ themeToggleButton.addEventListener('click', () => {
   updateIcons(currentTheme);
 });
 
+simple_themeToggleButton.addEventListener('click', () => {
+  console.log('THEME SWITCHED')
+  // Toggle between themes
+  currentTheme = (currentTheme === 'light-theme') ? 'dark-theme' : 'light-theme';
+
+  // Update body class and save the new theme in localStorage
+  bodyElement.className = currentTheme; // directly set the class to currentTheme
+  localStorage.setItem('theme', currentTheme);
+
+  // Update icon based on the new theme
+  updateIcons(currentTheme);
+});
+
 // Function to update the icon based on theme
 function updateIcons(theme) {
   if (theme === 'light-theme') {
-    themeIcon.classList.replace('fa-sun', 'fa-moon'); // Show moon icon for dark mode
+    // Show moon icon for dark mode
+    themeIcon.classList.replace('fa-sun', 'fa-moon');
+    simple_themeIcon.classList.replace('fa-sun', 'fa-moon');
   } else {
-    themeIcon.classList.replace('fa-moon', 'fa-sun'); // Show sun icon for light mode
+    // Show sun icon for light mode
+    themeIcon.classList.replace('fa-moon', 'fa-sun');
+    simple_themeIcon.classList.replace('fa-moon', 'fa-sun');
   }
 }
